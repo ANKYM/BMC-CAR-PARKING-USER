b110b64f1fe463de2082fb43c7de0b80b52535f9
yogesh2591 <yogesh2591@gmail.com>
2018-02-01 01:40:28 +0530

    Commit Message

---
+++ app/src/main/java/com/omkar/bmccarparkinguser/CustomInfoWindowGoogleMap.java
@@ -0,0 +1,30 @@
+package com.omkar.bmccarparkinguser;
+
+import android.content.Context;
+import android.view.View;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.model.Marker;
+
+/**
+ * Created by Yogesh on 01-02-2018.
+ */
+
+public class CustomInfoWindowGoogleMap implements GoogleMap.InfoWindowAdapter  {
+
+    private Context context;
+
+    public CustomInfoWindowGoogleMap(Context ctx){
+        context = ctx;
+    }
+
+    @Override
+    public View getInfoWindow(Marker marker) {
+        return null;
+    }
+
+    @Override
+    public View getInfoContents(Marker marker) {
+        return null;
+    }
+}
